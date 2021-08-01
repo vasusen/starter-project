812bcef29a1d9c4310a66f7e40742c33b09ea49e
Vasusen Patil <vasusen@gmail.com>
2021-07-31 23:29:41 -0700

    routing working

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Switch, Link } from "react-router-dom";
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
 
-function App() {
-  return (
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
-     <div>
-     <nav>
-         <ul id="navigation">
-             <li>
-                 <Link to="/">Home</Link>
-             </li>
-             <li>
-                 <Link to="/about">About</Link>
-             </li>
-             <li>
-                 <Link to="/contact">Contact</Link>
-             </li>
-         </ul>
-     </nav>
-</div>
-  );
+class App extends React.Component {
+  render() {
+      return (
+        <div className="App">
+          <div>
+            <nav>
+              <ul id="navigation">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/about">About</Link>
+                </li>
+                <li>
+                <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+            <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+          </div>
+            );
+  }
 }
 
 export default App;
---
+++ src/Contact.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const divStyle = {
+    color:'white'
+};
+
+class Contact extends React.Component {
+    
+    render() {
+        return (
+            <div style={divStyle}>
+                <h2>Contact Page</h2>
+                <main>
+                    <p>This section contains contact info...</p>
+                </main>
+            </div>
+        )
+    }
+}
+
+
+
+export default Contact;
